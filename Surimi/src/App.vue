c8bce415d6b577e3b9efd810a5bd7cb887c673e8
Akmot9 <avicocyprien@yahoo.com>
2023-08-20 16:40:09 +0200

    ménage

--- Surimi/src/App.vue
+++ Surimi/src/App.vue
@@ -1,11 +1,6 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import { invoke } from '@tauri-apps/api'
 
-// à présent, nous pouvons appeler notre commande !
-// Clique droite sur l'arrière-plan de l'application et ouvre les outils de développement.
-// Vous devriez apercevoir "Hello, World !" dans la console !
 invoke('greet', { name: 'World' })
   // `invoke` renvoie une promesse (Promise)
   .then((response) => console.log(response))
@@ -13,43 +8,15 @@ invoke('greet', { name: 'World' })
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
+    
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <div>hello</div>
+      <div>{hello}</div>
     </div>
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
